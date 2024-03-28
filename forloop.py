402c68bab573c6ed4f1c24ca5cfec966cee7f17f
Piyushnegi04 <piyushsinghnegi1@gmail.com>
2024-03-28 16:28:52 +0530

    for loop in python...

---
+++ forloop.py
@@ -0,0 +1,37 @@
+#for loop can  be used to iterate over a sequence (such as list, tuple or string) 
+# x ="avi"
+# for  i in x:
+#     print(i)
+
+x="avi"
+for i in reversed(x):
+    print(i)
+
+# for   i in range(1, 11, 2):  ##1  is the start value and 11 is the end value. And 2 skip value
+#     print(i)
+
+# for   i in reversed(range(1, 11)):
+#     print(i)
+
+#list
+# names = ["Avi", "John", "Jane"]
+# for name in names:
+#     if name == "Avi":
+#         print("Hello Avi")
+#         break
+#     else:
+#         print("record not found")
+   
+# names = ["Avi", "John", "Jane"]
+# surname =["singh","negi"]
+# for name in names:
+#     if name == "Avi":
+#         continue
+#         print("Hello Avi")
+#     else:
+#         print("record not found")
+
+#nested for loop
+# for x in names:
+#     for y in surname:
+#         print(x,y)
